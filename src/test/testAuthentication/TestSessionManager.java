bdcbaf0088c7ccc4c18c2caf225e6552eb984f71
Aleksandar Gađanski <157562336+agadjanski@users.noreply.github.com>
2024-11-28 20:27:03 +0800

    Update TestSessionManager.java

--- src/test/testAuthentication/TestSessionManager.java
+++ src/test/testAuthentication/TestSessionManager.java
@@ -4,70 +4,117 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import main.authentication.*;
-import main.exceptions.ExUserDoesNotExist;
-import main.users.*;
+import main.exceptions.ExInvalidPassword;
 
+public class TestPassword {
+    @Test
+    public void testValidPassword_1(){
+        Password password = new Password("AStrongPa55word");
 
-public class TestSessionManager {
+        try{
+            Password.validPassword(password);
+        } catch (Exception e){
+            fail("The password checker should not throw an excpetion, but it threw: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testValidPassword_2(){
+        Password password = new Password("short");
+        ExInvalidPassword e = assertThrows(ExInvalidPassword.class, () -> {
+            Password.validPassword(password);
+        });
+        assertEquals("Password must contain at least 6 characters\n", e.getMessage());
+    }
     
     @Test
-    public void testCreateSession(){
-        SessionManager sessionManager = SessionManager.getInstance();
-        int oldCount = sessionManager.getSize();
-        Password password = new Password("AStrongPa55word");
-        User user = new Customer("Kazakhstan", password);
+    public void testValidPassword_3(){ //invalid password (No uppercase letters)
+        Password password = new Password("123456137b");
 
-        sessionManager.createSession(user);
-        assertEquals(sessionManager.getSize(), oldCount + 1);
-    }
-
-    @Test
-    public void testRemoveSession_1(){
-        SessionManager sessionManager = SessionManager.getInstance();
-        int oldCount = sessionManager.getSize();
-        Password password = new Password("AStrongPa55word");
-        User user = new Customer("Kazakhstan", password);
-
-        sessionManager.createSession(user);
-        try {
-            sessionManager.removeSession(user);
-            assertEquals(sessionManager.getSize(), oldCount);
-        } catch (Exception e){
-            fail("Remove session should not throw an exception, but it threw: " + e.getMessage());
-        }
-    }
-
-    @Test
-    public void testRemoveSession_2(){
-        SessionManager sessionManager = SessionManager.getInstance();
-        Password password = new Password("AStrongPa55word");
-        User user = new Customer("Kazakhstan", password);
-        User newUser = new Customer("ThisUserDoesNotExist", password);
-
-        sessionManager.createSession(user);
-
-        assertThrows(ExUserDoesNotExist.class, () -> {
-            sessionManager.removeSession(newUser);
+        ExInvalidPassword e = assertThrows(ExInvalidPassword.class, () -> {
+            Password.validPassword(password);
         });
-    }
-    /*
-    @Test
-    public void testRemoveSession_3(){
-    	SessionManager sessionManager = SessionManager.getInstance();
-        int oldCount = sessionManager.getSize();
-        Password password = new Password("AStrongPa55word");
-        User user = new Customer("Kazakhstan", password);
 
-        sessionManager.createSession(user);
-        
-        try {
-        	sessionManager.removeSession(user);
-        } catch (Exception e) {
-        	fail("Remove session should not throw an exception, but it threw: " + e.getMessage());
-        }
-        
-        assertThrows(ExUserDoesNotExist.class, () -> {
-            sessionManager.removeSession(user);
+        assertEquals("Password must contain at least one uppercase letter\n", e.getMessage());
+    }
+
+    @Test
+    public void testValidPassword_4(){ //invalid password (no lowercase letters)
+        Password password = new Password("1234567123D");
+
+        ExInvalidPassword e = assertThrows(ExInvalidPassword.class, () -> {
+            Password.validPassword(password);
         });
-    }*/
+        assertEquals("Password must contain at least one lowercase letter\n", e.getMessage());
+    }
+
+    @Test
+    public void testRegister_5(){ //invalid password (no digits)
+        String parole = "abcdEFGHI";
+        Password password = new Password(parole);
+
+        ExInvalidPassword e = assertThrows(ExInvalidPassword.class, () -> {
+            Password.validPassword(password);
+        });
+
+        assertEquals("Password must contain at least one digit\n", e.getMessage());
+    }
+
+    @Test
+    public void testRegister_6(){ //invalid password (only digits)
+        String parole = "123456789";
+        Password password = new Password(parole);
+
+        ExInvalidPassword e = assertThrows(ExInvalidPassword.class, () -> {
+            Password.validPassword(password);
+        });
+
+        assertEquals("Password must contain at least one uppercase letter and at least one lowercase letter\n", e.getMessage());
+    }
+
+    @Test
+    public void testRegister_7(){ //invalid password (only lowercase)
+        String parole = "abcdefghijkl";
+        Password password = new Password(parole);
+
+        ExInvalidPassword e = assertThrows(ExInvalidPassword.class, () -> {
+            Password.validPassword(password);
+        });
+
+        assertEquals("Password must contain at least one digit and at least one uppercase letter\n", e.getMessage());
+    }
+
+    @Test
+    public void testRegister_8(){ //invalid password (only uppercase)
+        String parole = "ABCDEFGHIJKL";
+        Password password = new Password(parole);
+
+        ExInvalidPassword e = assertThrows(ExInvalidPassword.class, () -> {
+            Password.validPassword(password);
+        });
+
+        assertEquals("Password must contain at least one digit and at least one lowercase letter\n", e.getMessage());
+    }
+
+    @Test
+    public void testRegister_9(){ //invalid password (completely invalid)
+        String parole = "{-+-+-+-+-+-+";
+        Password password = new Password(parole);
+
+        ExInvalidPassword e = assertThrows(ExInvalidPassword.class, () -> {
+            Password.validPassword(password);
+        });
+
+        assertEquals("Password must contain at least one digit, at least one uppercase letter, and at least one lowercase letter\n", e.getMessage());
+    }
+    
+    @Test
+    public void testEquals() {
+    	Password password1 = new Password("Password1");
+    	Password password2 = new Password("Password2");
+    	Password password3 = new Password("Password1");
+    	
+    	assertTrue(password1.equals(password3));
+    	assertFalse(password1.equals(password2));
+    }
 }
